cc4f00e7c90679f862f1d469b82bb984c7637bf3
David <dgrayboff@gmail.com>
2016-06-19 22:29:58 -0400

    all forwards working. Backwards sort of working

--- assets/js/chess.js
+++ assets/js/chess.js
@@ -24,7 +24,6 @@
 
 // VARIABLES FOR EACH PIECE //
 
-var count = 0;
 var gp4 = $('.goldpawn4');
 var blank1 = $('.blank1');
 var bk2 = $('.bk2');
@@ -60,6 +59,7 @@ var blanktest = $('.blanktest');
 
 ////////////////////////////
 
+  var count = 0;
 
   $('.forwards').on('click', function(e) {
     count++;
@@ -76,15 +76,16 @@ var blanktest = $('.blanktest');
       blank1.append(move1);
       console.log("First move");
 
-        $('.backwards').on('click', function(e) {
-          if (count == 1 ){
-          // blank1.remove();
-          blank1.empty(); // empties the square of the previous move
-          gp4.append(); // try to append original html here
-          // gp4.append(move1);
-          console.log("reverse");
-        }
-        });
+        // $('.backwards').on('click', function(e) {
+        //
+        //   if (count == 1 ){
+        //   // blank1.remove();
+        //   blank1.empty(); // empties the square of the previous move
+        //   gp4.append(); // try to append original html here
+        //   // gp4.append(move1);
+        //   console.log("reverse");
+        // }
+        // });
 
 
     };
@@ -99,6 +100,19 @@ var blanktest = $('.blanktest');
       var move2 = board[2][5];
       blank2.append(move2);
       console.log("Second move");
+
+    //   $('.backwards').on('click', function(e) {
+    //
+    //     if (count == 2 ){
+    //     // blank1.remove();
+    //     blank2.empty(); // empties the square of the previous move
+    //     bk2.append(); // try to append original html here
+    //     // gp4.append(move1);
+    //     console.log("reverse");
+    //   }
+    // });
+
+
     };
 
     if (count == 3) {
@@ -130,9 +144,8 @@ var blanktest = $('.blanktest');
       board[6][6] = ' ';
       console.log(board.join('\n'));
       var move5 = board[5][6];
-      blank5.html(move5);
-
-      gp7.html(" ");
+      blank5.append(move5);
+      $('.seventh').html(" ");
       console.log("Fifth move");
     };
 
@@ -151,23 +164,24 @@ var blanktest = $('.blanktest');
 
     if (count == 7) {
 
-      var boardTwo = [
-        ['R','N','B','Q','K',bb2,bk2,'R'],
-        ['P','P','P',bp4,bp5,'P','P','P'],
-        [' ',' ',' ',' ',blank5,blank2,' ',' '],
-        [' ',' ',' ',blank6,' ',' ',' ',' '],
-        [' ',' ',blank3,blank1,' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        ['p','p',gp3,gp4,'p','p',blank7,'p'],
-        ['r','n','b','q','k',gb2,'test','r'] ];
+      // var boardTwo = [ // 7th move wasn't working as it moves to a place where a piece already was, so thought a new board would help, but found that appending using the div class was the trick instead of the image class that has the piece icon.
+      //   ['R','N','B','Q','K',bb2,bk2,'R'],
+      //   ['P','P','P',bp4,bp5,'P','P','P'],
+      //   [' ',' ',' ',' ',blank5,blank2,' ',' '],
+      //   [' ',' ',' ',blank6,' ',' ',' ',' '],
+      //   [' ',' ',blank3,blank1,' ',' ',' ',' '],
+      //   [' ',' ',' ',' ',' ',' ',' ',' '],
+      //   ['p','p',gp3,gp4,'p','p',blank7,'p'],
+      //   ['r','n','b','q','k',gb2,'test','r'] ];
 
-      console.log(boardTwo.join('\n') + '\n\n');
-      boardTwo[6][6] = boardTwo[7][5];
-      boardTwo[7][5] = ' ';
-      console.log(boardTwo.join('\n'));
+      console.log(board.join('\n') + '\n\n');
+      board[6][6] = board[7][5];
+      board[7][5] = ' ';
+      console.log(board.join('\n'));
       console.log(gp7);
-      var move7 = boardTwo[6][6];
-      blank7.html("<img src='images/png/bishop2.png'/>");
+      var move7 = board[6][6];
+      $('.seventh').append(move7);
+      $('.fifth').html(" ");
       // blank7.html(gb2);
       // blank7.append(board[6][6]);
       // gp7.append(gb2);
@@ -180,8 +194,8 @@ var blanktest = $('.blanktest');
       board[1][4] = board[0][5];
       board[0][5] = ' ';
       console.log(board.join('\n'));
-      board.join('\n');
-      blank8.html(bb2);
+      var move8 = board[1][4];
+      $('.eight').append(move8);
       console.log("Eighth move");
     };
 
@@ -197,28 +211,106 @@ var blanktest = $('.blanktest');
     };
   });
 
-// BACKWARDS CLICK //
 
-// var countback = 0;
-//
-// $('.backwards').on('click', function(e) {
-//   countback++;
-//   e.preventDefault;
-//
-//     // if (countback == 1) {
-//     //   console.log("Back click");
-//     //   board[6][3] = board[4][3];
-//     //   board[4][3] = ' ';
-//     //   var moveback1 = board[6][3];
-//     //   gp4.append(moveback1);
-//     // };
-//
-//     console.log("Back Click");
-//     console.log(board.join('\n') + '\n\n');
-//     board[4][3] = board[6][3];
-//     board[6][3] = ' ';
-//     console.log(board.join('\n'));
-//     var move1 = board[4][3];
-//     blank1.remove(move1);
-//     console.log("First move");
-//   });
+
+    // BACKWARDS CLICK //
+
+    var board2 = [
+      ['R','N','B','Q','K',bb2,bk2,'R'],
+      ['P','P','P',bp4,bp5,'P','P','P'],
+      [' ',' ',' ',' ',blank5,blank2,' ',' '],
+      [' ',' ',' ',blank6,' ',' ',' ',' '],
+      [' ',' ',blank3,blank1,' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      ['p','p',gp3,gp4,'p','p',gp7,'p'],
+      ['r','n','b','q','k',gb2,gk2,'r'] ];
+
+
+    $('.backwards').on('click', function(e) {
+
+      e.preventDefault;
+
+        // if (countback == 1) {
+        //   console.log("Back click");
+        //   board[6][3] = board[4][3];
+        //   board[4][3] = ' ';
+        //   var moveback1 = board[6][3];
+        //   gp4.append(moveback1);
+        // };
+
+
+        if (count == 1 ) {
+          count--;
+
+          var back1 = board2[6][3];
+          blank1 = ' ';
+          $('.goldpawn4b').html(back1); // does the trick, calling div instead of img
+          console.log("reverse");
+        }
+
+        if (count == 2 ){
+          count--;
+
+          var back2 = board2[0][6];
+          // blank2 = ' ';
+          $('.knight2').html(back2);
+          console.log("reverse");
+        }
+
+        if (count == 3 ){
+          count--;
+
+          var back3 = board2[6][2];
+          $('.pawn3b').html(back3);
+          console.log("reverse");
+        }
+
+        if (count == 4 ){
+          count--;
+
+          var back4 = board2[1][4];
+          $('.pawn5b').html(back4);
+          console.log("reverse");
+        }
+
+        if (count == 5 ){
+          count--;
+
+          var back5 = board2[6][6];
+          $('.seventh').html(back5);
+          console.log("reverse");
+        }
+
+        if (count == 6 ){
+          count--;
+
+          var back6 = board2[1][3];
+          $('.blackpawn4b').html(back6);
+          console.log("reverse");
+        }
+
+        if (count == 7 ){
+          count--;
+
+          var back7 = board2[7][5];
+          $('.fifth').html(back7);
+          console.log("reverse");
+        }
+
+        if (count == 8 ){
+          count--;
+
+          var back8 = board2[0][5];
+          $('.blackbishopb').html(back8);
+          console.log("reverse");
+        }
+
+        if (count == 9 ){
+          count--;
+
+          var back9 = board2[7][6];
+          $('.goldknightb').html(back9);
+          console.log("reverse");
+        }
+
+});
